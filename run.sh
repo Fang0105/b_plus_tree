a8763763dac35cba3af84253a614da8f74c733d3
Fang0105 <nody0105@gmail.com>
2023-11-29 14:50:54 +0000

    use valgrind to check whether all memories used in b+ tree are release

--- run.sh
+++ run.sh
@@ -10,3 +10,11 @@ make
 diff -s key_query_out.txt key_query_ans.txt
 diff -s range_query_out.txt range_query_ans.txt
 cat time_used.txt
+
+which valgrind
+if [ "$?" -ne 0 ]
+then
+    echo "valgrind command not found"
+else
+    valgrind --leak-check=full ./hw2
+fi
